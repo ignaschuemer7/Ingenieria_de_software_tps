103cbbd8148d19401ec069fa25a68dcfd34b7613
Santiago Tomas Torres <101135886+storres0514@users.noreply.github.com>
2025-03-19 23:28:54 -0300

    tests and comments ready

--- tp1/Tests.hs
+++ tp1/Tests.hs
@@ -17,6 +17,7 @@ testF action = unsafePerformIO $ do
         isException :: SomeException -> Maybe ()
         isException _ = Just ()
 
+
 -- Definición de datos de prueba
 
 -- Palets
@@ -58,18 +59,18 @@ flatTruck = newT 10 0 r1
 
 
 -- Lista de pruebas
-pruebas =
+tests =
     [ 
-      -- Pruebas de Palet
+      ----- Pruebas de Palet -----
       testF (newP "Buenos Aires" (-1)), -- Debería fallar, el peso no puede ser negativo
       not (testF (newP "Buenos Aires" 1)), -- No debería fallar
 
-      -- Pruebas de Route
+      ----- Pruebas de Route -----
       not (testF (newR [])), -- No debería fallar, una ruta puede ser vacía
       inOrderR r1 "Buenos Aires" "Córdoba",
       not (inOrderR r1 "Córdoba" "Buenos Aires"),
 
-      -- Pruebas de Stack
+      ----- Pruebas de Stack -----
       freeCellsS s1 == 5,
       freeCellsS s2 == 4,
       freeCellsS s5 == 1,
@@ -77,7 +78,7 @@ pruebas =
       holdsS s1 p1 r1,
       not (holdsS s2 p3 r1),
 
-      -- Pruebas de Truck
+      ----- Pruebas de Truck -----
       testF (newT (-1) 5 r1), -- Debería fallar, la cantidad de bahías no puede ser negativa
       testF (newT 5 (-1) r1), -- Debería fallar, la altura de las bahías no puede ser negativa
       not (testF (newT 0 0 r1)), -- No debería fallar, un camión puede tener 0 bahías y 0 altura
@@ -96,26 +97,46 @@ pruebas =
       freeCellsT (unloadT (loadT (loadT (loadT t1 p1) p2) p3) "Rosario") == 33,
       freeCellsT (unloadT (unloadT (loadT (loadT (loadT t1 p1) p2) p3) "Rosario") "Buenos Aires") == 34,
 
-      -- Carga de múltiples palets
+
+      ----- Carga de múltiples palets -----
+
+      -- p7 + p7 + p6 + p7 + p6 = 5 cargas -> celdas libres = 12 (capacidad) - 5 (cargas) = 7
       freeCellsT (loadT (loadT (loadT (loadT (loadT t2 p7) p7) p6) p7) p6) == 7,
+      -- 3 + 3 + 4 + 3 + 4 = 17 toneladas
       netT (loadT (loadT (loadT (loadT (loadT t2 p7) p7) p6) p7) p6) == 17,
 
+      -- p7 + p7 + p6 + p7 + p6 + p5 + p5 = 7 cargas -> celdas libres = 12 (capacidad) - 7 (cargas) = 5
       freeCellsT (loadT (loadT (loadT (loadT (loadT (loadT (loadT t2 p7) p7) p6) p7) p6) p5) p5) == 5,
+
+      -- p7 + p7 + p6 + p7 + p6 + p5 + p5 + p6 = 7 cargas ya que el último palet p6 no puede ser cargado -> celdas libres = 12 - 7 = 5
       freeCellsT (loadT (loadT (loadT (loadT (loadT (loadT (loadT (loadT t2 p7) p7) p6) p7) p6) p5) p5) p6) == 5,
 
-      -- Descarga en orden de ruta
+
+      ----- Descarga en orden de ruta -----
+
+      -- p7 + p7 + p6 + p7 + p6 + p5 - p5 = 5 cargas -> celdas libres = 12 - 5 = 7
       freeCellsT (unloadT (loadT (loadT (loadT (loadT (loadT (loadT (loadT t2 p7) p7) p6) p7) p6) p5) p5) "Madrid") == 7,
+
+      -- p7 + p7 + p6 + p7 + p6 + p5 + p5 = 7 cargas - p5 - p5 - p6 - p6 = 3 cargas -> celdas libres = 12 - 3 = 9
       freeCellsT (unloadT (unloadT (loadT (loadT (loadT (loadT (loadT (loadT (loadT t2 p7) p7) p6) p7) p6) p5) p5) "Madrid") "Barcelona") == 9,
+      -- 3 * p7 = 3 * 3 = 9 toneladas
+      netT (unloadT (unloadT (loadT (loadT (loadT (loadT (loadT (loadT (loadT t2 p7) p7) p6) p7) p6) p5) p5) "Madrid") "Barcelona") == 9,
+
+      -- se cargan y se descargan todos los palets -> celdas libres = 12 = capacidad del camión
       freeCellsT (unloadT (unloadT (unloadT (loadT (loadT (loadT (loadT (loadT (loadT (loadT t2 p7) p7) p6) p7) p6) p5) p5) "Madrid") "Barcelona") "Valencia") == 12,
 
-      -- Intento de cargar un palet que excede el peso permitido
-      netT (loadT t2 p9) == 0
+      -- Intento de cargar un palet que excede el peso permitido (10 toneladas)
+      netT (loadT t2 p9) == 0,
+
+      -- Peso Truck vacío + peso de la carga = peso del camión con una carga
+      netT t2 + netP p7 == netT (loadT t2 p7)
+
     ]
 
 -- Evaluación de los tests
 main :: IO ()
 main = do
-    let resultados = zip [1..] pruebas
-    mapM_ (\(n, r) -> putStrLn $ "Test " ++ show n ++ ": " ++ if r then "OK" else "FALLÓ") resultados
-    putStrLn $ "All tests passed: " ++ show (and pruebas)
+    let results = zip [1..] tests
+    mapM_ (\(n, r) -> putStrLn $ "Test " ++ show n ++ ": " ++ if r then "OK" else "FAILED") results
+    putStrLn $ "All tests passed: " ++ show (and tests)
     return ()
